7ebe58f6eb1ee897420898d48ad3f989ef3b1ac8
Karl <karlito1998@hotmail.com>
2021-06-26 20:51:32 -0400

    Generate Animated Graphs

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab;
+
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
+    private Transform[] points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float step = 2f / resolution;
+        Vector3 position = Vector3.zero;
+        Vector3 scale = Vector3.one * step;
+        points = new Transform[resolution];
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i] = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
+    }
+}
